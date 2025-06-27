d84e228ea4ca4d24ea8c578e3d850b9cac21af2c
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-27 11:45:44 +0200

    Create client.js

---
+++ pages/client.js
@@ -0,0 +1,8 @@
+export default function ClientPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold text-blue-600">Bienvenue sur votre Espace Client</h1>
+      <p className="mt-4">Vous pourrez bientôt créer et suivre vos tickets de réclamation ici.</p>
+    </div>
+  );
+}
